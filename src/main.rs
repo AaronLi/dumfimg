efeb27b793d9a9c76cb0bd4c78e12cb65db2de4f
AaronLi <dumfing@gmail.com>
2022-12-06 21:05:59 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+mod image_view;
+
+use std::collections::{HashMap, HashSet};
+use std::env::args;
+use std::process::exit;
+use std::thread::sleep;
+use std::time::Duration;
+use clap::Parser;
+use cursive::{Cursive, CursiveExt};
+use cursive::views::{DebugView, ResizedView};
+use image::{imageops, ImageResult};
+use image::imageops::FilterType;
+use image::io::Reader as ImageReader;
+use crate::image_view::ImageView;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    input_image: String
+}
+
+fn main() {
+    let args = Args::parse();
+    let image = match ImageReader::open(args.input_image) {
+        Ok(image) => {
+            match image.decode() {
+                Ok(image_data) => {
+                    image_data.to_rgb8()
+                }
+                Err(e) => {
+                    println!("Failed to read image data: {}", e);
+                    exit(1);
+                }
+            }
+        }
+        Err(e) => {
+            println!("Failed to open image: {}", e);
+            exit(1);
+        }
+    };
+    let mut base = Cursive::new();
+    base.add_fullscreen_layer(ResizedView::with_full_screen(ImageView::new(image)));
+    base.run();
+}
