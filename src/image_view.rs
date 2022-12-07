afb3cf384212dccef364be26037a39ff82548d4b
AaronLi <dumfing@gmail.com>
2022-12-06 22:41:08 -0500

    Fix aspect ratio and zoom algorithm

--- src/image_view.rs
+++ src/image_view.rs
@@ -13,7 +13,8 @@ pub struct ImageView {
     scaled_image: Option<RgbImage>,
     relayout: bool,
     mode: ImageViewMode,
-    filter_mode: FilterType
+    filter_mode: FilterType,
+    rendered_view: Option<[[f32; 2]; 2]>
 }
 
 enum ImageViewMode {
@@ -30,16 +31,23 @@ impl View for ImageView {
                 printer.print((printer.output_size.x/2, printer.output_size.y/2), "Error")
             }
             Some(im) => {
+                if let Some(v) = self.rendered_view {
+                    if v == self.view {
+                        return;
+                    }
+                }
                 let draw_start_time = SystemTime::now();
+                let left_padding = printer.output_size.x.checked_sub(im.width() as usize).unwrap_or(0)/2;
+                let top_padding = printer.output_size.y.checked_sub(im.height() as usize).unwrap_or(0)/2;
                 for (row, row_pixels) in im.rows().enumerate() {
                     let mut color = [0, 0, 0];
                     let mut amount = 0;
                     let mut start_pos = 0;
                     for (column, pixel) in row_pixels.map(|c|c.0).enumerate() {
-                        if ImageView::color_distance(&pixel, &color) < 5 {
+                        if ImageView::color_distance(&pixel, &color) < 3 {
                             amount += 1;
                         }else{
-                            printer.with_color(ColorStyle::front(ColorType::Color(Color::Rgb(color[0], color[1], color[2]))), |printer|{printer.print((start_pos, row), &"█".repeat(amount))});
+                            printer.with_color(ColorStyle::front(ColorType::Color(Color::Rgb(color[0], color[1], color[2]))), |printer|{printer.print((start_pos + left_padding, row + top_padding), &"█".repeat(amount))});
                             amount = 1;
                             color = pixel;
                             start_pos = column;
@@ -51,7 +59,7 @@ impl View for ImageView {
                             }
                         }
                     }
-                    printer.with_color(ColorStyle::front(ColorType::Color(Color::Rgb(color[0], color[1], color[2]))), |printer|{printer.print((start_pos, row), &"█".repeat(amount))});
+                    printer.with_color(ColorStyle::front(ColorType::Color(Color::Rgb(color[0], color[1], color[2]))), |printer|{printer.print((start_pos + left_padding, row + top_padding), &"█".repeat(amount))});
                 }
                 printer.print((0, 0), &format!("{}ms", draw_start_time.elapsed().unwrap().as_secs_f32()*1000f32))
             }
@@ -92,7 +100,13 @@ impl View for ImageView {
         let view_left = self.view[0][0] * self.source_image.width() as f32;
         let view_top = self.view[1][0] * self.source_image.height() as f32;
         let crop_image = crop_imm(&self.source_image, view_left.round() as u32, view_top.round() as u32, view_width.round() as u32, view_height.round() as u32).to_image();
-        self.scaled_image = Some(resize(&crop_image, size.x as u32, size.y as u32, self.filter_mode));
+        let image_aspect_ratio = self.source_image.width() as f32 / self.source_image.height() as f32;
+        let window_aspect_ratio = size.x as f32 / (size.y as f32 * 2.5f32);
+        if image_aspect_ratio < window_aspect_ratio {
+            self.scaled_image = Some(resize(&crop_image, (2.5f32 * image_aspect_ratio * size.y as f32) as u32, size.y as u32, self.filter_mode));
+        }else {
+            self.scaled_image = Some(resize(&crop_image, size.x as u32, (1f32/(2.5f32 * image_aspect_ratio) * size.x as f32) as u32, self.filter_mode));
+        }
     }
 
     fn needs_relayout(&self) -> bool {
@@ -178,6 +192,10 @@ impl View for ImageView {
                                 position.x -= 1;
                                 EventResult::Consumed(None)
                             },
+                            ImageViewMode::ZOOM => {
+                                self.mode = ImageViewMode::MOVE;
+                                EventResult::Consumed(None)
+                            }
                             _ => EventResult::Ignored
                         }
                     },
@@ -196,6 +214,10 @@ impl View for ImageView {
                                 position.x += 1;
                                 EventResult::Consumed(None)
                             },
+                            ImageViewMode::ZOOM => {
+                                self.mode = ImageViewMode::MOVE;
+                                EventResult::Consumed(None)
+                            }
                             _ => EventResult::Ignored
                         }
                     }
@@ -238,6 +260,7 @@ impl ImageView {
             relayout: false,
             mode: ImageViewMode::MOVE,
             filter_mode: FilterType::Nearest,
+            rendered_view: None
         }
     }
 
@@ -246,11 +269,12 @@ impl ImageView {
     }
 
     fn zoom(&mut self, amount: f32) {
-        let (width, height) = self.view_size();
+        let (zoom_size, _) = self.view_size();
         let min_dimension = 1f32/self.source_image.width().min(self.source_image.height()) as f32;
-        let new_view = [[(self.view[0][0] + width * amount).clamp(0f32, 1f32), (self.view[0][1] - width * amount).clamp(0f32, 1f32)], [(self.view[1][0] + height * amount).clamp(0f32, 1f32), (self.view[1][1] - height * amount).clamp(0f32, 1f32)]];
-        let new_width = new_view[0][1] - new_view[0][0];
-        if new_view != self.view && new_width > min_dimension {
+        let view_center = ((self.view[0][0] + self.view[0][1])/2f32, (self.view[1][0] + self.view[1][1])/2f32);
+        let new_zoom_size = (zoom_size - zoom_size * amount).clamp(min_dimension, 1f32);
+        let new_view = [[view_center.0 - new_zoom_size/2f32, view_center.0 + new_zoom_size/2f32], [view_center.1 - new_zoom_size/2f32, view_center.1 + new_zoom_size/2f32]];
+        if new_view != self.view {
             self.view = new_view;
             self.relayout = true;
         }
